e1fdbbeaa0cd2dcea474da392525c78f00813a25
uwi0 <kakapoweb@gmail.com>
2024-08-27 15:48:47 +0700

    Initial Commit

---
+++ Gradient Indicator/Gradient_IndicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gradient_IndicatorApp.swift
+//  Gradient Indicator
+//
+//  Created by dwi prasetyo on 27/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Gradient_IndicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
